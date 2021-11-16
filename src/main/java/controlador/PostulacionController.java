0bd1c6881a8dcbc0333991400fd42661e511965c
Matias Escobar <matias.escobar@mercadolibre.com>
2021-11-16 10:27:54 -0300

    Add object mapper

--- src/main/java/controlador/PostulacionController.java
+++ src/main/java/controlador/PostulacionController.java
@@ -1,5 +1,6 @@
 package controlador;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import domains.Postulacion;
 import dtos.PostulacionDTO;
 import interfaces.PostulacionService;
@@ -7,9 +8,15 @@ import interfaces.PostulacionService;
 public class PostulacionController {
 
     private PostulacionService postulacionService;
+    private ObjectMapper objectMapper;
+
+    public PostulacionController(PostulacionService postulacionService, ObjectMapper objectMapper) {
+        this.postulacionService = postulacionService;
+        this.objectMapper = objectMapper;
+    }
 
     public String postularse(PostulacionDTO postulacionDTO) throws Exception {
-        Postulacion postulacion = new Postulacion();
+        Postulacion postulacion = this.objectMapper.convertValue(postulacionDTO, Postulacion.class);
         return postulacionService.postularse(postulacion,  postulacionDTO.getId());
     }
 }
