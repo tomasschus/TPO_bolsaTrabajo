17f71ebd22bf43d306805687dfafbc4cd7203578
Tomas Schuster <ts2525@hotmail.com>
2021-11-18 00:16:34 -0300

    InformesController getOfertasLaboralesConMasPublicaciones

--- src/main/java/controlador/InformesController.java
+++ src/main/java/controlador/InformesController.java
@@ -1,6 +1,58 @@
 package controlador;
 
+import domains.Empresa;
+import domains.OfertaLaboral;
+import java.util.ArrayList;
+
 public class InformesController {
     //• La oferta laboral que más postulantes recibió en un mes y/o año determinado.
+    ArrayList<Empresa> empresas = EmpresaController.getInstanceEmpresas();
+
+    public ArrayList<OfertaLaboral> getOfertaLaborales(){
+        ArrayList<OfertaLaboral> ofertasLaborales = new ArrayList<>();
+        for (Empresa em: empresas){
+            for(OfertaLaboral of: em.getOfertasLaborales()){
+                ofertasLaborales.add(of);
+            }
+        }
+        return ofertasLaborales;
+    }
     
+    public OfertaLaboral getOfertaLaboralConMasPostulantes(int mes, int año){
+        OfertaLaboral masPostulantes = null;
+        for (Empresa em: empresas) {
+            for (OfertaLaboral of : em.getOfertasLaborales()) {
+                if(of.getFechaPublicacion().getYear()==año && of.getFechaPublicacion().getMonthValue()==mes){
+                    if(masPostulantes==null){
+                        masPostulantes=of;
+                    }
+                    else{
+                        if(of.getPostulantes().size()>masPostulantes.getPostulantes().size()){
+                            masPostulantes=of;
+                        }
+                    }
+                }
+            }
+        }
+        return masPostulantes;
+    }
+    public OfertaLaboral getOfertaLaboralConMasPostulantes(int año){
+        OfertaLaboral masPostulantes = null;
+        for (Empresa em: empresas) {
+            for (OfertaLaboral of : em.getOfertasLaborales()) {
+                if(of.getFechaPublicacion().getYear()==año){
+                    if(masPostulantes==null){
+                        masPostulantes=of;
+                    }
+                    else{
+                        if(of.getPostulantes().size()>masPostulantes.getPostulantes().size()){
+                            masPostulantes=of;
+                        }
+                    }
+                }
+            }
+        }
+        return masPostulantes;
+    }
+
 }
